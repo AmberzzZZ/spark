075e6f2e8293716106ccdc5a2be14de6300a519b
amber.zhang <774524217@qq.com>
2020-09-16 15:33:55 +0800

    modification

--- metric.py
+++ metric.py
@@ -5,9 +5,8 @@ import SimpleITK as sitk
 import math
 
 
-def cal_metrics(gt_lst, pred_lst, cls_id):    # per case
-    in_distance = []
-    case_TP, case_FP, case_FN = 0
+def cal_metrics(gt_lst, pred_lst, cls_id, in_distance):    # per case
+    case_TP, case_FP, case_FN = 0, 0, 0
     for gt_kp in gt_lst:
         gt_x, gt_y, spacing_x, spacing_y = gt_kp
 
@@ -18,8 +17,6 @@ def cal_metrics(gt_lst, pred_lst, cls_id):    # per case
             # 2. other matched: skip
             # 3. not matched: FP
             # 4. without match: FN
-            # 5. distance >=6 for all: FP
-
             pred_x, pred_y, cls = pred_kp
             dis = math.sqrt(((gt_x-pred_x)*spacing_x)**2+((gt_y-pred_y)*spacing_y)**2)
             if dis<6:
@@ -60,7 +57,7 @@ def match_slice(data_dir, seriesUid, instanceUid):
             # read pixel spacing
             spacing = image.GetMetaData('0028|0030') if image.HasMetaDataKey('0028|0030') else None
             if spacing:
-                spacing = map(float, spacing.split('\\'))
+                spacing = list(map(float, spacing.split('\\')))
             return dcm, spacing
     return None, None
 
@@ -68,7 +65,7 @@ def match_slice(data_dir, seriesUid, instanceUid):
 def match_pred(annotations_pred, studyUid):
     for anno in annotations_pred:
         if anno['studyUid'] == studyUid:
-            return anno['data']
+            return anno['data'][0]
     return None
 
 
@@ -86,7 +83,6 @@ if __name__ == '__main__':
     annotations_pred = json.loads(f.read())
     f.close()
 
-
     TP, FP, FN = [0 for i in range(7)], [0 for i in range(7)], [0 for i in range(7)]
     # tranverse per case
     for anno_per_case in annotations_gt:
@@ -98,13 +94,13 @@ if __name__ == '__main__':
         slice, spacing = match_slice(folder, slice_info['seriesUid'], slice_info['instanceUid'])
         if not slice or not spacing:
             continue
-        kpoints = slice_info['point']
-        gt_lst = [[] for i in range(7)]   # [bone-v1, bone-v2, disc-v1, disc-v2, disc-v3, disc-v4, disc-v5]
+        kpoints = slice_info['annotation'][0]['data']['point']
+        gt_lst = [[] for i in range(7)]   # [vertebra-v1, vertebra-v2, disc-v1, disc-v2, disc-v3, disc-v4, disc-v5]
         for point in kpoints:
-            if 'bone' in point['tag'].keys():
-                if point['tag']['bone']=='v1':
+            if 'vertebra' in point['tag'].keys():
+                if point['tag']['vertebra']=='v1':
                     cls_id = 0
-                if point['tag']['bone']=='v2':
+                if point['tag']['vertebra']=='v2':
                     cls_id = 1
             if 'disc' in point['tag'].keys():
                 if point['tag']['disc']=='v1':
@@ -117,7 +113,7 @@ if __name__ == '__main__':
                     cls_id = 5
                 if point['tag']['disc']=='v5':
                     cls_id = 6
-            gt_lst[cls_id].append(point['tag']['coord']+spacing)
+            gt_lst[cls_id].append(point['coord']+spacing)
 
         # read pred
         slice_info_pred = match_pred(annotations_pred, anno_per_case['studyUid'])
@@ -126,10 +122,10 @@ if __name__ == '__main__':
         kpoints = slice_info_pred['point']
         pred_lst = []
         for point in kpoints:
-            if 'bone' in point['tag'].keys():
-                if point['tag']['bone']=='v1':
+            if 'vertebra' in point['tag'].keys():
+                if point['tag']['vertebra']=='v1':
                     cls_id = 0
-                if point['tag']['bone']=='v2':
+                if point['tag']['vertebra']=='v2':
                     cls_id = 1
             if 'disc' in point['tag'].keys():
                 if point['tag']['disc']=='v1':
@@ -142,16 +138,20 @@ if __name__ == '__main__':
                     cls_id = 5
                 if point['tag']['disc']=='v5':
                     cls_id = 6
-            pred_lst.append(point['tag']['coord']+[cls_id])
+            pred_lst.append(point['coord']+[cls_id])
 
         # cal metric per case per cls
+        in_distance = []
         for i in range(7):
             if not gt_lst[i]:
                 continue
-            case_TP, case_FP, case_FN = cal_metrics(gt_lst[i], pred_lst, i)
+            case_TP, case_FP, case_FN = cal_metrics(gt_lst[i], pred_lst, i, in_distance)
             TP[i] += case_TP
             FP[i] += case_FP
             FN[i] += case_FN
+        # 5. distance >=6 for all: FP
+        if len(pred_lst) != len(in_distance):
+            FP[i] += len(pred_lst) - len(in_distance)
 
     # cal recall/precision/f1 per cls
     recall = [0 for i in range(7)]
